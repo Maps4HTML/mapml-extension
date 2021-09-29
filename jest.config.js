ae33110c1de3d0a98374bbb78702521e00d7a775
Ahmad Ayubi <aayub041@uottawa.ca>
2021-09-28 21:58:31 -0400

    Update package.json and test driver

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,6 @@ module.exports = {
     },
     {
       displayName: "E2E Testing",
-      globals: {
-        PATH: "http://localhost:3000/",
-        ISHEADLESS: true
-      },
       testMatch: ["**/test/e2e/**/*.test.js"]
     }
   ]
