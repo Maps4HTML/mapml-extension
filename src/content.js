ae730bc531bff78b4587b5edfec48edbfaf646d1
Ahmad Ayubi <aayub041@uottawa.ca>
2021-10-14 12:58:16 -0400

    Use map-options element rather than setting M.options directly

--- src/content.js
+++ src/content.js
@@ -1,10 +1,9 @@
-let options = {};
-
 /**
- * Adds the js/options.js script to the DOM
+ * Appends the map-options element to the documents head, used to initialize M.options
  */
-function loadOptionsScript() {
-  if(document.querySelector("mapml-viewer") || document.querySelector("map[is='web-map']")) {
+document.addEventListener("readystatechange", () => {
+  // customElements registry is not defined by this point
+  if (document.querySelector("mapml-viewer") || document.querySelector("map[is='web-map']")) {
     /*
     TODO: add when you want to remove network requests for built in version
     let viewerScript = document.querySelector('script[src="https://unpkg.com/@maps4html/web-map-custom-element@latest/dist/mapml-viewer.js"]');
@@ -12,29 +11,17 @@ function loadOptionsScript() {
       viewerScript.src = chrome.runtime.getURL("/js/mapml-viewer.js");
     }*/
     chrome.storage.local.get("options", function (o) {
-      options = o.options || {};
-      let script = document.createElement('script');
-      script.src = chrome.runtime.getURL("/js/options.js");
-      document.head.appendChild(script);
+      let mapOptions = document.createElement("map-options");
+      mapOptions.innerHTML = JSON.stringify(o.options || {});
+      document.head.appendChild(mapOptions);
     });
   }
-}
+}, {once: true});
 
 /**
- * Handler used to add options.js script to the DOM on readyState 'interactive'
- */
-let loadOptionsScriptHandler = function () {
-  if (document.readyState === "interactive") {
-    loadOptionsScript();
-    document.removeEventListener("readystatechange", loadOptionsScriptHandler);
-  }
-};
-document.addEventListener("readystatechange", loadOptionsScriptHandler);
-
-/**
- * posts message passing the options, the listener for this message
- * is found in the injected script
+ * Removes the map-options element from the document head after it's been used
  */
 document.addEventListener("DOMContentLoaded", () => {
-  window.postMessage({type:"set-options", options: options}, "*");
+  let mapOptions = document.head.querySelector("map-options");
+  if (mapOptions) document.head.removeChild(mapOptions);
 }, {once: true});
--- src/popup.js
+++ src/popup.js
@@ -5,7 +5,7 @@ let options = {};
  */
 function saveOptions() {
   chrome.storage.local.set({
-    options : options,
+    options: options,
   });
 }
 
@@ -13,12 +13,12 @@ function saveOptions() {
  * Loads the users options from storage and sets it to the global (global to this file) options object
  * it also updates the respective elements to reflect the current state
  */
-function loadOptions(){
-  chrome.storage.local.get("options",function (o){
+function loadOptions() {
+  chrome.storage.local.get("options", function (o) {
     options = o.options || {};
-    for(let name in options){
+    for (let name in options) {
       let elem = document.getElementById(name);
-      if(elem){
+      if (elem) {
         switch (typeof options[name]) {
           case "boolean":
             elem.checked = options[name];
@@ -59,7 +59,7 @@ function resetStorage() {
 /**
  * Attaches event handlers to the user inputs (i.e. checkboxes), and initializes the options
  */
-document.addEventListener("DOMContentLoaded",  () => {
+document.addEventListener("DOMContentLoaded", () => {
   loadOptions();
   document.getElementById("announceMovement").addEventListener("change", handleCheckboxChange);
   document.getElementById("clear").addEventListener("click", resetStorage);
