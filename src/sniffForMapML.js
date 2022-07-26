3c69f1c51b9c8cb07417b5308a2278a6d2b5e2a9
ben-lu-uw <b54lu@uwaterloo.ca>
2022-07-26 11:19:39 -0400

    Re-add toggle

--- src/sniffForMapML.js
+++ src/sniffForMapML.js
@@ -1,4 +1,7 @@
 let mapml = document.querySelector("mapml-");
 if(mapml) {
-    chrome.runtime.sendMessage("hasMapML");
+    chrome.storage.local.get("options", function (items) {
+        let generateMap = items.options ? items.options.generateMap : false;
+        if (generateMap) chrome.runtime.sendMessage("hasMapML");
+    });
 }
