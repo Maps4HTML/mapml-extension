ddcf51c6b9ca50c7b8fc77a6ae4622ae00c01115
ben-lu-uw <b54lu@uwaterloo.ca>
2022-07-26 12:02:15 -0400

    Create map from wrapped text/mapml

--- src/sniffForMapML.js
+++ src/sniffForMapML.js
@@ -1,7 +1,19 @@
-let mapml = document.querySelector("mapml-");
-if(mapml) {
+function sendMessage() {
     chrome.storage.local.get("options", function (items) {
         let renderMap = items.options ? items.options.renderMap : false;
         if (renderMap) chrome.runtime.sendMessage("hasMapML");
     });
+}
+
+let mapml = document.querySelector("mapml-");
+if(mapml) {
+    sendMessage()
+} else {
+    //Browser wraps text/mapml with a pre element
+    let pre = document.querySelector("pre");
+    if(pre && document.contentType === "text/mapml") {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(pre.innerText, "application/xml");
+        if(doc.querySelector("mapml-")) sendMessage();
+    }
 }
